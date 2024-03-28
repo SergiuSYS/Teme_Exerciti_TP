3c3388a87075ee3e69d3caa58ff3112e6c75e179
Sergiu <sergiusay06@gmail.com>
2024-03-28 18:00:31 +0200

    tema5

---
+++ tema6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int verificare(){
+
+}
+int parcurgere(int vector[], int size){
+    int i =0;
+    printf("%d",vector[i]);
+    if (i == size)
+        return;
+    return parcurgere(vector, i++);
+    
+}
+int main(){
+    int vector[]= {1,2,3,4,5,6,7,8,9};
+    int size = sizeof(vector) / sizeof(vector[0]);
+    parcurgere(vector,size);
+
+    return 0;
+}
