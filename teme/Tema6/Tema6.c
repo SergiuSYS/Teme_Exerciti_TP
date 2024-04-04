b6b273e5c07786c6abb145e96c8493763f75eb3f
Sergiu <sergiusay06@gmail.com>
2024-04-04 13:33:28 +0300

    ok

--- teme/Tema6/Tema6.c
+++ teme/Tema6/Tema6.c
@@ -1,47 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int Suma_numar(int numar) {
-  int suma = 0;
+int verificare(){
 
-  if (numar == 0)
-    return suma;
-
-  int rest = numar % 10;
-  suma += rest;
-
-  return suma + Suma_numar(numar / 10);
 }
+int parcurgere(int vector[], int size){
+    int i =0;
+    printf("%d",vector[i]);
+    if (i == size)
+        return -1;
+    return parcurgere(vector, i++);
+    
+}
+int main(){
+    int vector[]= {1,2,3,4,5,6,7,8,9};
+    int size = sizeof(vector) / sizeof(vector[0]);
+    parcurgere(vector,size);
 
-int invers_numar(int numar) {
-  if (numar == 0)
     return 0;
-
-  int numar_inversat = 0;
-
-  while (numar != 0) {
-    int rest = numar % 10;
-    numar_inversat = numar_inversat * 10 + rest;
-    numar /= 10;
-  }
-
-  return numar_inversat;
-}
-
-int parcurgere_inlocuire(int vector[], int size, int i) {
-  if (i >= size)
-    return -1;
-  if (Suma_numar(vector[i]) % 2 == 0) {
-    vector[i] = invers_numar(vector[i]);
-  }
-
-  printf("%d, ", vector[i]);
-  return parcurgere_inlocuire(vector, size, i + 1);
-}
-
-int main() {
-  int vector[] = {20, 30, 14, 15, 12};
-  int size = sizeof(vector) / sizeof(vector[0]);
-  parcurgere_inlocuire(vector, size, 0);
-
-  return 0;
 }
