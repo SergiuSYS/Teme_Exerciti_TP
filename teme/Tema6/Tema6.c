0a738e53703cb500d860138608acc6a4703b2359
Sergiu <sergiusay06@gmail.com>
2024-04-24 16:11:25 +0300

    daaa

--- teme/Tema6/Tema6.c
+++ teme/Tema6/Tema6.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 
 int Suma_numar(int numar) {
-  int suma = 0;
+    int suma = 0;
+  
+    if (numar == 0)
+        return suma;
 
-  if (numar == 0)
-    return suma;
+    int rest = numar % 10;
+    suma += rest;
+  
+    return suma + Suma_numar(numar / 10);
+}
 
-  int rest = numar % 10;
-  suma += rest;
+int invers_numar_rec(int numar, int reversedNum) {
+    if (numar == 0)
+        return reversedNum;
 
-  return suma + Suma_numar(numar / 10);
+    int lastDigit = numar % 10;
+
+    reversedNum = (reversedNum * 10) + lastDigit;
+
+    return invers_numar_rec(numar / 10, reversedNum);
 }
 
 int invers_numar(int numar) {
-  if (numar == 0)
-    return 0;
-
-  int numar_inversat = 0;
-
-  while (numar != 0) {
-    int rest = numar % 10;
-    numar_inversat = numar_inversat * 10 + rest;
-    numar /= 10;
-  }
-
-  return numar_inversat;
+    return invers_numar_rec(numar, 0);
 }
 
-int parcurgere_inlocuire(int vector[], int size, int i) {
-  if (i >= size)
-    return -1;
-  if (Suma_numar(vector[i]) % 2 == 0) {
-    vector[i] = invers_numar(vector[i]);
-  }
 
-  printf("%d, ", vector[i]);
-  return i + parcurgere_inlocuire(vector, size, i + 1);
+void parcurgere_inlocuire(int vector[], int size, int i) {
+    if (i >= size)
+        return;
+
+    if (Suma_numar(vector[i]) % 2 == 0) {
+        vector[i] = invers_numar(vector[i]);
+    }
+
+    printf("%d, ", vector[i]);
+    parcurgere_inlocuire(vector, size, i + 1);
 }
 
 int main() {
-  int vector[] = {20, 30, 14, 15, 12};
-  int size = sizeof(vector) / sizeof(vector[0]);
-  parcurgere_inlocuire(vector, size, 0);
+    int vector[] = {20, 30, 14, 15, 12};
+    int size = sizeof(vector) / sizeof(vector[0]);
+    parcurgere_inlocuire(vector, size, 0);
 
-  return 0;
+    return 0;
 }
--- teme/Tema8/Tema8.c
+++ teme/Tema8/Tema8.c
@@ -1,72 +1,84 @@
-#include<stdio.h>
-#define MAX 1000
+    #include<stdio.h>
+    #include<stdlib.h>
+    #define MAX 1000
 
-// CHEMAREA FUNCTIILOR NECESARE PENTRU ALGORITM
+    // CHEMAREA FUNCTIILOR NECESARE PENTRU ALGORITM
 
-void swap(int*x, int*y); // functie pentru interschimbare
-void quickSort(int array[], int lenght); // functie pentru utilizator pentru a nu a fi nevoit sa introduca valuare minima si maxima
-void quickSort_recursivitate(int array[], int low, int high); //algoritmul de quick sort in sine 
-int partition(int array[], int low, int high); 
+    void swap(int*x, int*y); // functie pentru interschimbare
+    void quickSort(int array[], int lenght); // functie pentru utilizator pentru a nu a fi nevoit sa introduca valuare minima si maxima
+    void quickSort_recursivitate(int array[], int low, int high); //algoritmul de quick sort in sine 
+    int partition(int array[], int low, int high); 
 
 int main(){
-    int j = 1,num,a[MAX],lenght;
-    FILE * arrayInput;
+    int j = 0, num, a[MAX], length;
+    int *sir = NULL; // Inițializează sir cu NULL
 
-    // IMPORTANT in fisier primul numar va fi marimea sa
-    arrayInput = fopen("inputArray.txt", "r");
+    FILE *arrayInput = fopen("inputArray.txt", "r");// in fisier se v a trece un numar pe fiecare linie 
 
-    fscanf(arrayInput, "%d", &lenght);
-    while (fscanf(arrayInput, "%d", &num) == 1 && j < MAX) {
-        a[j] = num;
-        j++;
+    if (arrayInput == NULL) {
+        printf("Eroare la deschiderea fisierului.");
+        return -1;
     }
 
-    quickSort(a,lenght);
+    while (fscanf(arrayInput, "%d", &num) == 1 && j < MAX) {
+        sir = realloc(sir, (j + 1) * sizeof(int)); // Realocă memoria pentru sir
+        if (sir == NULL) {
+            printf("Eroare la realocarea memoriei.");
+            return -1;
+        }
+        sir[j] = num;
+        j++;
+    }
+    length = j;
 
-    for (int i = 0; i < lenght; i++)
-        printf("%d ", a[i]);
+    quickSort(sir, length);
 
+    for (int i = 0; i < length; i++)
+        printf("%d ", sir[i]);
+
+    free(sir); // Eliberează memoria alocată pentru sir
     fclose(arrayInput);
     return 0;
 }
 
+    void swap(int *x,int *y){
 
-void swap(int *x,int *y){
-
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void quickSort(int array[], int lenght){
-
-    quickSort_recursivitate(array, 0, lenght-1);
-}
-
-void quickSort_recursivitate(int array[], int low, int high){
-    if (low < high)
-    {
-        int pivot_index = partition(array, low, high);
-        quickSort_recursivitate(array, low,pivot_index-1);
-        quickSort_recursivitate(array, pivot_index+1, high);
+        int temp = *x;
+        *x = *y;
+        *y = temp;
     }
-       
-}
 
-int partition(int array[], int low, int high){
+    void quickSort(int array[], int lenght){
 
-    int pivot_value = array[high];
+        quickSort_recursivitate(array, 0, lenght-1);
+    }
 
-    int i = low; 
-    for (int j = low; j < high; j++)
-    {
-        if (array[j] <= pivot_value)
+    void quickSort_recursivitate(int array[], int low, int high){
+        if (low < high)
         {
-            swap(&array[i], &array[j]);
-            i++;
+            int pivot_index = partition(array, low, high);
+            quickSort_recursivitate(array, low,pivot_index-1);
+            quickSort_recursivitate(array, pivot_index+1, high);
         }
+        
     }
-    swap(&array[i],&array[high]);
 
-    return i;
-}
+    int partition(int array[], int low, int high){
+
+        int pivot_value = array[high];
+
+        int i = low; 
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] >= pivot_value)
+            {
+                swap(&array[i], &array[j]);
+                i++;
+            }
+        }
+        swap(&array[i],&array[high]);
+
+        return i;
+    }
+
+
